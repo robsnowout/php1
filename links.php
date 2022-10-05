5b1a29a28813249b1facc285b0edf2b534771adb
robsnowout <snowhouse1234@outlook.com>
2022-10-05 04:31:09 -0400

    created PHP link

---
+++ links.php
@@ -0,0 +1,29 @@
+snowhouse1234@out
+robsnowout
+
+ <?php
+        $conn=mysqli_connect("localhost", "showcase", "showcase123", "na723_Showcase");
+        // Check connection
+        if ($conn-> connect_error) {
+         die ("Connection failed:". $conn-> connect_error);
+        }
+
+        $sql = "Select Title, Fname, Lname,url from Showcase";
+        $result = $conn-> query($sql);
+        if ($result-> num_rows > 0) {
+            While ($row = $result-> fetch_assoc())  {
+                echo "<tr>";
+//echo "<td>",'<a href=' . $row['url'] . '>$title </a>',"</td>";  
+echo "<td>",'<a href=' . $row['url'] . '>'.$row['Title'].' </a>',"</td>";
+
+            }
+            Echo "</table>";
+        }
+        else {
+            echo "0 result";
+        }
+
+
+$conn-> close();
+
+?>
